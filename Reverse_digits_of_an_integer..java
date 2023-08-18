6bd6c052026dfed9be9bfbd3f871eb2500378632
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-08-18 11:40:47 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args) {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int rev=0,rem,original=n;         while(n!=0){             rem=n%10;             rev=rev*10+rem;             n=n/10;         }
               System.out.println(rev);
           
        }
    }

---
+++ Reverse_digits_of_an_integer..java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0,rem,original=n;
+        while(n!=0){
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+       
+           System.out.println(rev);
+       
+    }
+}
