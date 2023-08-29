8f17aa02fd363680412bcc65b1c1153ff2cd3f31
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-08-29 20:06:11 +0530

    import java.util.Scanner;
    public class NextPrimePalindrome {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            int inputNumber = scanner.nextInt();
            
            if (inputNumber >= 10 && inputNumber <= 10000) {
                int nextPrimePalindrome = findNextPrimePalindrome(inputNumber);
                System.out.println(nextPrimePalindrome);
            } else {
                System.out.println("Input number must be between 10 and 10000 (inclusive).");
            }
            
            scanner.close();
        }
    
        public static int findNextPrimePalindrome(int num) {
            while (true) {
                num++;
                if (isPalindrome(num) && isPrime(num)) {
                    return num;
                }
            }
        }
    
        public static boolean isPalindrome(int num) {
            int original = num;
            int reversed = 0;
    
            while (num > 0) {
                int digit = num % 10;
                reversed = reversed * 10 + digit;
                num /= 10;
            }
    
            return original == reversed;
        }
    
        public static boolean isPrime(int num) {
            if (num <= 1) {
                return false;
            }
            if (num <= 3) {
                return true;
            }
            if (num % 2 == 0 || num % 3 == 0) {
                return false;
            }
    
            for (int i = 5; i * i <= num; i += 6) {
                if (num % i == 0 || num % (i + 2) == 0) {
                    return false;
                }
            }
            
            return true;
        }
    }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,33 +1,59 @@
 import java.util.Scanner;
-public class Main{
-    static boolean isPrime(int n)
-    {
-        if (n <= 1)
-            return false;
-        for (int i = 2; i <= Math.sqrt(n); i++)
-            if (n % i == 0)
-                return false;
-  
-        return true;
-    }
-  static boolean checkPalindrome(int n)
-  {
-    int reverse = 0;
-    int temp = n;
-    while (temp != 0) {
-      reverse = (reverse * 10) + (temp % 10);
-      temp = temp / 10;
-    }
-    return (reverse == n);
-  }
+
+public class NextPrimePalindrome {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        for(int i=x+1;;i++){
-            if(isPrime(i)==true&&checkPalindrome(i)==true){
-                System.out.println(i);
-                break;
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        
+        if (inputNumber >= 10 && inputNumber <= 10000) {
+            int nextPrimePalindrome = findNextPrimePalindrome(inputNumber);
+            System.out.println(nextPrimePalindrome);
+        } else {
+            System.out.println("Input number must be between 10 and 10000 (inclusive).");
+        }
+        
+        scanner.close();
+    }
+
+    public static int findNextPrimePalindrome(int num) {
+        while (true) {
+            num++;
+            if (isPalindrome(num) && isPrime(num)) {
+                return num;
             }
         }
+    }
+
+    public static boolean isPalindrome(int num) {
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
