eb4e205ca9439a81e993e03a992023b3dfa2f022
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-09-14 20:31:07 +0530

    import java.util.*; public class Main{ 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int n=sc.nextInt(); 		int arr[]=new int[n]; 		int sum=0,avg,c=0; 		for(int i=0;i<n;i++){ 		    arr[i]=sc.nextInt(); 		    sum+=arr[i]; 		} 		avg=sum/n; 		for(int i=0;i<n;i++){ 		    if(arr[i]<=avg){ 		        c++; 		    } 		} 		System.out.println(c); 	} }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		int sum=0,avg,c=0;
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		    sum+=arr[i];
+		}
+		avg=sum/n;
+		for(int i=0;i<n;i++){
+		    if(arr[i]<=avg){
+		        c++;
+		    }
+		}
+		System.out.println(c);
+	}
+}
