329763f3401fcefb31878c2b77ad8603450e512e
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-09-16 12:30:14 +0530

    import java.util.Scanner; public class TwoDimensionalArrayExample {     public static void main(String[] args) {         Scanner sc=new Scanner(System.in);         int R,C;         R=sc.nextInt();         C=sc.nextInt();         //create a 2-D array which is capable of         //storing R rows and C columns         int[][] arr=new int[R][C];         //reading         for (int i=0;i<R;i++){//i runs on rows             for (int j=0;j<C;j++){//j runs on columns                 arr[i][j]=sc.nextInt();             }         }         //accessing         int sum=0;         for (int i=1;i<R-1;i++){//i runs on rows             for (int j=1;j<C-1;j++){//j runs on columns                 //System.out.println(arr[i][j]*arr[i][j]+" ");                 //if(i!=0 && j!=0 && i!=R-1 && j!=R-1){                     sum+=arr[i][j];                     //continue;                 //}             }           // System.out.println();         }         System.out.println(sum);     } }

---
+++ Sum_of_non_boundary_values_in_a_matrix.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class TwoDimensionalArrayExample {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int R,C;
+        R=sc.nextInt();
+        C=sc.nextInt();
+        //create a 2-D array which is capable of
+        //storing R rows and C columns
+        int[][] arr=new int[R][C];
+        //reading
+        for (int i=0;i<R;i++){//i runs on rows
+            for (int j=0;j<C;j++){//j runs on columns
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        //accessing
+        int sum=0;
+        for (int i=1;i<R-1;i++){//i runs on rows
+            for (int j=1;j<C-1;j++){//j runs on columns
+                //System.out.println(arr[i][j]*arr[i][j]+" ");
+                //if(i!=0 && j!=0 && i!=R-1 && j!=R-1){
+                    sum+=arr[i][j];
+                    //continue;
+                //}
+            }
+          // System.out.println();
+        }
+        System.out.println(sum);
+    }
+}
