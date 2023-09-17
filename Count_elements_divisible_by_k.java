263866382e70bb3724f6b75a6e4e7f2052d5b229
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-09-17 19:10:05 +0530

    import java.util.*; public class Main{ 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int n=sc.nextInt(); 		int k=sc.nextInt(); 		int arr[]=new int[n]; 		//int brr[]=new int[n]; 		int sum=0,avg,c=0; 		for(int i=0;i<n;i++){ 		    arr[i]=sc.nextInt(); 		} 		for(int i=0;i<n;i++){ 		    if(arr[i]%k==0){ 		        /*System.out.print(arr[i]); 		        break;*/ 		        c++; 		    }
    		}
    		System.out.println(c);
    	}
    }

--- Count_elements_divisible_by_k.java
+++ Count_elements_divisible_by_k.java
@@ -4,18 +4,17 @@ public class Main{
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int m=sc.nextInt();
+		int k=sc.nextInt();
 		int arr[]=new int[n];
-		int brr[]=new int[n];
+		//int brr[]=new int[n];
 		int sum=0,avg,c=0;
 		for(int i=0;i<n;i++){
 		    arr[i]=sc.nextInt();
-		    //sum+=arr[i];
 		}
 		for(int i=0;i<n;i++){
-		    if(arr[i]%m==0){
-		        //System.out.print(arr[i]);
-		        //break;
+		    if(arr[i]%k==0){
+		        /*System.out.print(arr[i]);
+		        break;*/
 		        c++;
 		    }
 		    
