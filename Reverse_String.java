c6d4b3a68c36a1bfc424ce1ccc4fc3a932697a32
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-09-20 06:59:33 +0530

    import java.util.*; public class reverseString {         public static void main(String[] args) {             Scanner sc=new Scanner(System.in);             String s=sc.nextLine();             String[] arr=s.split(" ");             for(int i=arr.length-1;i>=0;i--){                 /*if(arr[i]==(char)32){                     continue;                 }*/                 System.out.print(arr[i]+" ");             }         }     }

--- Reverse_String.java
+++ Reverse_String.java
@@ -3,9 +3,12 @@ public class reverseString {
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             String s=sc.nextLine();
-            char[] arr=s.toCharArray();
+            String[] arr=s.split(" ");
             for(int i=arr.length-1;i>=0;i--){
-                System.out.print(arr[i]);
+                /*if(arr[i]==(char)32){
+                    continue;
+                }*/
+                System.out.print(arr[i]+" ");
             }
         }
     }
