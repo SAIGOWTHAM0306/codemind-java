7747fb63aa1a254b75dbc1c1d7c7b574025ab443
SAIGOWTHAM0306 <119730087+SAIGOWTHAM0306@users.noreply.github.com>
2023-09-21 06:29:59 +0530

    import java.util.*; public class Main{ 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int n=sc.nextInt(); 		int arr[]=new int[n]; 		int brr[]=new int[n]; 		int sum=0,avg; 		for(int i=0;i<n;i++){ 		    arr[i]=sc.nextInt(); 		} 		int min=arr[0]; 		for(int i=0;i<n;i++){ 		    if(arr[i]<min){ 		        min=arr[i]; 		    } 		} 		System.out.println(min); 	} }

--- Minimum_element_of_an_array.java
+++ Minimum_element_of_an_array.java
@@ -4,19 +4,18 @@ public class Main{
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int[] arr=new int[n];
+		int arr[]=new int[n];
+		int brr[]=new int[n];
+		int sum=0,avg;
 		for(int i=0;i<n;i++){
 		    arr[i]=sc.nextInt();
 		}
-		int max=arr[0];
-// 		for(int i=0;i<n;i++){
-// 		    sum=sum+arr[i];
-// 		}
-        for(int eachele:arr){
-            if(eachele<max){
-                max=eachele;
-            }
-        }
-		System.out.println(max);
+		int min=arr[0];
+		for(int i=0;i<n;i++){
+		    if(arr[i]<min){
+		        min=arr[i];
+		    }
+		}
+		System.out.println(min);
 	}
 }
